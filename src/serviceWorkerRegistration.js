60233b2e753a4401bafed823c3be4aa70f345991
Mani4212 <manikanigiri1029@gmail.com>
2021-07-05 15:22:16 +0530

    intialized git and installed dependencies

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,3 +1,7 @@
+/* eslint-disable */
+
+import { disable } from "workbox-navigation-preload";
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
