5d9c91172ec6f8b8ee56ff66ea747180fcaefa3a
Mani4212 <manikanigiri1029@gmail.com>
2021-07-13 12:25:18 +0530

    using custom hooks for starred shows

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,13 +1,13 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const FlexGrid=styled.div`
-display: flex;
-justify-content: center;
-flex-wrap: wrap;
+export const FlexGrid = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 
-export const SearchCard=styled.div`
-width: 300px;
+export const SearchCard = styled.div`
+  width: 300px;
   height: 100%;
   margin: 0 15px 40px;
 
@@ -24,6 +24,7 @@ width: 300px;
       width: 100%;
     }
   }
+
   h1 {
     margin: 10px 0;
     font-size: 21px;
@@ -33,11 +34,12 @@ width: 300px;
     margin: 0;
   }
 `;
+
 export const Star = styled.div`
   display: inline-block;
   width: 18px;
   height: 18px;
-  background-color: #ffc806;
+  background-color: ${props => (props.active ? '#ffc806' : '#ddd')};
   clip-path: polygon(
     50% 0%,
     61% 35%,
